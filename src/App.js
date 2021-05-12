90ec5020fa3a3330b0965c962a3e874e98b24389
saleor <saleor@guug.it>
2021-05-12 16:53:54 +0000

    intro

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 import cn from 'classnames';
 
 import './custom.css';
-import s from './App.modules.scss'
+import s from './App.module.scss';
 
 const App = () => {
-    return (
-        <div className={cn(s.header, 'color')}>
-            Yes, we Did It! This is App Component!
-        </div>
-    )
-}
+  return <div className={cn(s.header, 'color')}>Yes, we Did It! This is App Component!</div>;
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,53 @@
 const path = require('path');
+const { resolve } = path;
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
-	resolve: {
-		extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-	},
-	mode: NODE_ENV ? NODE_ENV : 'development',
-	entry: path.resolve(__dirname, 'src/index.js'),
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.[tj]sx?$/,
-				use: ['ts-loader'],
-			},
-			{
-				test: /\.(s*)css$/,
-				use: [
-					'style-loader',
-					{
-						 loader: 'css-loader',
-						 options: {
-							 modules: {
-								 mode: 'local',
-								 localIdentName: '[name]__[local]__[hash:base64:5]',
-								 auto: /\.modules\.\w+$/i,
-							 }
-						 }
-					},
-					'sass-loader'
-				],
-			}
-		]
-	},
-	plugins: [
-		new HTMLWebpackPlugin({
-			template: path.resolve(__dirname, 'public/index.html'),
-		})
-	],
-	devServer: {
-		port: 3000,
-		open: true,
-		hot: true,
-	},
-	devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  mode: NODE_ENV ? NODE_ENV : 'development',
+  entry: resolve(__dirname, 'src/index.js'),
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+                auto: /\.module\.\w+$/i,
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  devtool: 'source-map',
 };
