51c13561d8f02bf54aea27bd0bac51972439446c
saleor <saleor@guug.it>
2021-05-17 07:50:36 +0000

    Heading refactoring

--- src/components/Heading /index.tsx
+++ src/components/Heading /index.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
 
-type Type = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+type HeaderType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+type HeaderNum = 1 | 2 | 3 | 4 | 5 | 6;
 interface IProps {
-  type: Type;
+  type: HeaderType | HeaderNum;
   children: React.ReactNode | undefined;
 }
 
 const Heading: React.FC<IProps> = ({ type, children }: IProps) => {
-  switch (type) {
-    case 'h1':
-      return <h1>{children}</h1>;
-    case 'h2':
-      return <h2>{children}</h2>;
-    case 'h3':
-      return <h3>{children}</h3>;
-    case 'h4':
-      return <h4>{children}</h4>;
-    case 'h5':
-      return <h5>{children}</h5>;
-    case 'h6':
-      return <h6>{children}</h6>;
-    default:
-      return null;
-  }
+  const Tag = `h${type}`.replace('hh', 'h') as keyof JSX.IntrinsicElements;
+  return <Tag>{children}</Tag>;
 };
 
 export default Heading;
