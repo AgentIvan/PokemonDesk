d1d4eb868da61fd409b6b39896ad40bcc9f08963
saleor <saleor@guug.it>
2021-05-30 19:45:13 +0000

    video 2,3

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,9 +3,28 @@ import { EndpointType } from '../config';
 import IQuery from '../interface/query';
 import getUrlWithParamsConfig from './getUrlWithParamsConfig';
 
+interface IOptions {
+  method: string;
+  body?: string;
+}
+
+// interface IGetUrlWithParamsConfig {
+//   method: string;
+//   uri: Partial<URL>;
+//   body: any;
+// }
+
 export const req = async <T>(endpoint: EndpointType, query?: IQuery): Promise<T> => {
-  const uri = Url.format(getUrlWithParamsConfig(endpoint, query));
-  return fetch(uri).then((response) => response.json());
+  const { method, uri, body } = getUrlWithParamsConfig(endpoint, query);
+  const options: IOptions = {
+    method,
+  };
+
+  if (Object.keys(body).length > 0) {
+    options.body = JSON.stringify(body);
+  }
+
+  return fetch(Url.format(uri), options).then((res) => res.json());
 };
 
 export default req;
